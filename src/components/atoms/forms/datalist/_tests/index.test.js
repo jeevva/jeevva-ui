b23f8fe4547c086abd68e4a6ab454ff093d9b5c2
jeevva <harun24896@gmail.com>
2024-01-21 00:16:57 +0700

    test: Testing component datalist

---
+++ src/components/atoms/forms/datalist/_tests/index.test.js
@@ -0,0 +1,18 @@
+import { render, screen } from '@testing-library/react';
+
+import Datalist from '..';
+
+/*
+ * Unit Testing
+ * Component Datalist
+ * Render Component
+ * Detect Text or childreen props
+ */
+
+describe('Component Test Get Text', () => {
+	it('Component Datalist', () => {
+		render(<Datalist>Hello world</Datalist>);
+		const text = screen.getByText('Hello world');
+		expect(text).toBeInTheDocument();
+	});
+});
