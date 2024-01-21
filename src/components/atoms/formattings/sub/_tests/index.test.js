d3a1b79c71771e87ee864be44b431545c7f4ea54
DevinTan4 <vninecoc1@gmail.com>
2024-01-21 14:34:54 +0700

    feat: remove unecessary comments

--- src/components/atoms/formattings/sub/_tests/index.test.js
+++ src/components/atoms/formattings/sub/_tests/index.test.js
@@ -1,11 +1,8 @@
 import { render, screen } from "@testing-library/react";
 import Sub from "..";
 
-// import Sub from './Sub';
-
 describe("Component Test Sub", () => {
   it("Component Render Sub", () => {
-    // render(<Sub children = "H2O"/>);
     render(<Sub>H2O</Sub>);
     const text = screen.getByText("H2O");
     expect(text).toBeInTheDocument();
@@ -17,11 +14,3 @@ describe("Component Test Sub", () => {
     expect(text).toBeInTheDocument();
   });
 });
-
-// describe('Component Test Get Text', () => {
-//   it('Component Datalist', () => {
-//     render(<Datalist>Hello world</Datalist>);
-//     const text = screen.getByText('Hello world');
-//     expect(text).toBeInTheDocument();
-//   });
-// })
